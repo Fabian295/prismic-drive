521c9f75b02eb4cf630e801e4448a37aa25d29f2
Fabian Kanhai <fabian.k@ziggo.nl>
2024-09-18 20:18:29 +0200

    Separate code client aand server side.Client code to AnimatedContent.tsx

--- fkwd-ai/src/slices/Hero/index.tsx
+++ fkwd-ai/src/slices/Hero/index.tsx
@@ -1,9 +1,8 @@
 import Bounded from "@/components/Bounded";
-import ButtonLink from "@/components/ButtonLink";
-import StarGrid from "@/components/StarGrid";
-import { Content, isFilled } from "@prismicio/client";
-import { PrismicNextImage } from "@prismicio/next";
-import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+
+import AnimatedContent from "./AnimatedContent";
+import { SliceComponentProps } from "@prismicio/react";
+import { Content } from "@prismicio/client";
 
 /**
  * Props for `Hero`.
@@ -20,36 +19,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="text-center"
     >
-      <div className="relative">
-        <StarGrid />
-        {isFilled.richText(slice.primary.heading) && (
-          <h1 className="text-balance text-cyan-200 text-4xl font-medium md:text-6xl">
-            <PrismicText field={slice.primary.heading} />
-          </h1>
-        )}
-        
-        {isFilled.richText(slice.primary.body) && (
-          <div className="mx-auto mt-6 text-slate-300 text-balance max-w-md md:max-w-4xl">
-            <PrismicRichText field={slice.primary.body} />
-          </div>
-        )}
-
-        {isFilled.link(slice.primary.button_link) &&  (
-          <ButtonLink className="mt-8"
-          field={slice.primary.button_link}>
-            {slice.primary.button_label}
-          </ButtonLink>
-        )}
-
-        {isFilled.image(slice.primary.image) && (
-          <div className="glass-container mt-16 w-fit">
-            <div className="absolute  inset-0 -z-10 bg-indigo-500/40 blur-2xl filter" />
-            <div className="bg-slate-950/60 p-8 rounded-xl">
-             <PrismicNextImage className="rounded-lg" field={slice.primary.image} />
-            </div>
-          </div>
-        )}
-      </div> 
+      <AnimatedContent slice={slice} />
     </Bounded>
   );
 };
